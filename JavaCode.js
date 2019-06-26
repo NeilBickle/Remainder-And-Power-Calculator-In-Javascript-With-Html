76a1d8c08b28fc4130c3e0fd3de700f1004c56b1
Neil Bickle (AMBCS) (AssocIScT) (TMIET) (ICTTech) <43986316+NeilBickle@users.noreply.github.com>
2019-06-26 09:26:18 +0100

    Ver 1.1

--- JavaCode.js
+++ JavaCode.js
@@ -2,6 +2,16 @@
     document.body.style.backgroundColor = "Beige";
     txtNumOne.focus();
     txtNumOne.scrollIntoView();
+    txtNumTwo.value = 0;
+    parInfo.innerHTML = 0;
+    var k = new Date();
+    var j = new Date();
+    var g = new Date();
+    var t = new Date();
+    var h = new Date();
+    var r = new Date();
+    var d = new Date()
+    parDate.innerHTML = "The Current Time And Date Of Logon To This Website Was:   " + t.getHours() + ":" + h.getMinutes() + ":" + r.getSeconds() + ":" + d.getMilliseconds() + "    -   " + g.getDate() + "/" + j.getMonth() + "/" + k.getFullYear();
 }
 
 function Title_onmouseover() {
